fc9059f94b36ee60e449f86efe12f50912214a32
Peter <piotrp92@gmail.com>
2020-10-26 04:50:08 +0000

    add clickerLooger

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function clickerLogger (event) {
-event.preventDefault()
-    console.log(event.target.classList.value)
-    console.log(event.srcElement.id)
+    event.preventDefault()
+ 
+    const atr = event.target
+
+     if(atr.getAttribute('src') || atr.getAttribute('href') || atr.tagName == 'BUTTON' ){
+        atr.getAttribute('class') != null ? console.log(`Class :${atr.getAttribute('class')}`) : null
+        atr.getAttribute('id') != null ? console.log(`ID :${atr.getAttribute('id')}`) : null
+     }
 }
 
 
